5390c32ddf3e458716266689f930c49eb5f8ee0b
Juuso Toivanen <juusotoivanen03@gmail.com>
2025-11-25 14:50:30 +0200

    filter.js tests

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -1,4 +1,4 @@
-import { expect } from "chai";
+import { AssertionError, expect } from "chai";
 import filter from "../src/filter.js";
 
 describe("filter()", () => {
@@ -20,6 +20,12 @@ describe("filter()", () => {
             category: ["Monitor", "Computer", "Display", "Electronics"],
             price: 199.99,
             components: ["Screen", "Stand", "Cable"]
+        },
+        product4: {
+            name: undefined,
+            category: [undefined],
+            price: NaN,
+            components: undefined
         }
     };
 
@@ -34,10 +40,62 @@ describe("filter()", () => {
     it('should return all products with x category name', () => {
         const productsArray = Object.values(testProducts);
         const result = filter(productsArray, product => product.category.includes("Electronics"));
-        console.log(result);
         expect(result).to.be.an('array').that.has.lengthOf(3);
-        //expect(result[0]).to.have.property('name', 'Thinkpad');
     });
 
+    it('should return an empty array', () => {
+        const productsArray = Object.values(testProducts);
+        const result = filter(productsArray, product => product.category.includes("ASDSA"));
+        expect(result).to.be.an('array').that.has.lengthOf(0);
+        
+    });
+
+    it('null as input should return an empty array', () => {
+        const result = filter(null, product => product.category.includes("ASDSA"));
+        expect(result).to.be.an('array').that.has.lengthOf(0);
+
+    });
+
+    it('undefined as input should return an empty array', () => {
+        const result = filter(undefined, product => product.category.includes("ASDSA"));
+        expect(result).to.be.an('array').that.has.lengthOf(0);
+    });
+
+    it('String as value should return TypeError', () => {
+        const text = "THIS IS EPIC"
+        expect(() => filter(text, product => product.category.includes("ASDSA"))).to.throw(TypeError);
+    });
+
+    it('Number as value should return empty array', () => {
+        const result = filter(12, num => num);
+        expect(result).to.be.an('array').that.has.lengthOf(0);
+    });
+
+    it('Object as value should return an empty array', () => {
+        //const productsArray = Object.values(testProducts);
+        const result = filter(testProducts, product => product.price > 500);
+        expect(result).to.be.an('array').that.has.lengthOf(0);
+    });
+
+
+    it('With no predicate should return TypeError', () => {
+        const productsArray = Object.values(testProducts);
+        expect(() => filter(productsArray)).to.throw(TypeError);
+        expect(() => filter(productsArray, null)).to.throw(TypeError);
+        expect(() => filter(productsArray, undefined)).to.throw(TypeError);
+        expect(() => filter(productsArray, "Not function")).to.throw(TypeError);
+    });
+
+    // Edge.
+    it('Empty array should return an empty array', () => {
+        const products = [];
+        const result = filter(products, product => product)
+        expect(result).to.be.an('array').that.has.lengthOf(0);
+    });
+
+    it('Product with undefined name should throw typeError', () => {
+        const productsArray = Object.values(testProducts);
+        expect(() => filter(productsArray, product => product.name.includes("Think"))).to.throw(TypeError);
+    });
 
 });
